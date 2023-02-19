56d438b3121c0eb14cb5d65dd87041097a949d86
Dmanthonyx <dmanthonycoding@gmail.com>
2023-02-19 14:23:20 -0800

    add functions to the dom w/ comments

--- js/app.js
+++ js/app.js
@@ -1,20 +1,55 @@
+// query dom for buttons
 const numberButtons = document.querySelectorAll('.number-btn')
-const addButton = document.querySelector('.add-btn');
+const arithmeticButtons = document.querySelectorAll('.arithmetic-btn');
+const calculateButton = document.querySelector('.calculate-btn');
+const clearButton = document.querySelector('.clear-btn');
+const positiveNegativeButton = document.querySelector('.positive-negative-btn');
 
-let currentValue = 0;
+// set the initial problem to 0
+let problem = 0;
 
-numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click', function(e) {
-        console.log(e.target, '<-- button')
-        const input = document.querySelector('input');
-        console.log(input.value, ' input value <--')
-        input.value = `${input.value}${e.target.innerText}`
-    })
-})
+// toggles negative/postitive numbers
+function toggleNegativePositive() {
+    const input = document.querySelector('input');
+    if(input.value.charAt(0) === '-')  input.value = input.value.slice(1)
+    if(input.value) input.value = `-${input.value}`
+}
 
-addButton.addEventListener('click', function(e) {
-    const input =  document.querySelector('input');
-    currentValue = input.value
+// clears the problem
+function clearProblem() {
+    const input = document.querySelector('input');
     input.value = null;
-    console.log(currentValue, 'current')
+    problem = 0;
+}
+
+// concatenates numbers to the problem
+function concatenateNumbers(e) {
+    const input = document.querySelector('input');
+    input.value = `${input.value}${e.target.innerText}`
+}
+
+// concatenates arithmetic operators to the problem
+function concatenateArithmeticOperator(e) {
+    const input =  document.querySelector('input');
+    if(!input.value) return
+    if(problem) {
+        problem += ` ${input.value} ${e.target.innerText}`
+    } else {
+        problem = `${input.value} ${e.target.innerText}`
+    }
+    input.value = null;
+}
+
+function calculateProblem() {
+
+}
+
+// addEventListeners to the dom
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener('click', concatenateNumbers)
 })
+arithmeticButtons.forEach((numberButton) => {
+    numberButton.addEventListener('click', concatenateArithmeticOperator)
+})
+clearButton.addEventListener('click', clearProblem)
+positiveNegativeButton.addEventListener('click', toggleNegativePositive)
